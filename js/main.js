47d160347f4d007702e8460187707950eb52c2a6
WragnarokA <wilmer.arango.a@gmail.com>
2023-05-11 17:59:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return{
+            il: "Il",
+            mio: "mio",
+            primo: "primo",
+            messaggio: "messaggio 😁",
+
+        }
+    },
+    methods: {
+
+    } 
+
+}).mount('#app')
+
+console.log("ciao CAZZZZOOOOO");
